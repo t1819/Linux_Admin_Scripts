2e1c218fd7b1800f97f0ed31c7abec9f4e1dee19
t1819 <Rits@Riteshs-MacBook-Pro.local>
2015-08-01 00:45:38 +0530

    Remote Folder Sync
    Remote Folder Sync using lftp tool.

---
+++ RemoteFolderSync/LftpSync.sh
@@ -0,0 +1,21 @@
+#!/bin/bash    
+
+HOST="ServerIP"
+USER="username"
+PASS="password"
+FTPURL="ftp://$USER:$PASS@$HOST"
+LCD="/path/of/your/local/dir"
+RCD="/path/of/your/remote/dir"
+
+DELETE="--delete"
+lftp -c "set ftp:list-options -a;
+open '$FTPURL';
+lcd $LCD;
+cd $RCD;
+mirror --reverse \
+      $DELETE \
+      --verbose \
+      --exclude-glob a-dir-to-exclude/ \
+      --exclude-glob a-file-to-exclude \
+      --exclude-glob a-file-group-to-exclude* \
+      --exclude-glob other-files-to-exclude"
